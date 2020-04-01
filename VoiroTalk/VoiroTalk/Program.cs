c3c8d0bd623b0349d0bc460dfe0124ac861d5f7e
XilonSakurane <epsilon_xjmptw0@yahoo.co.jp>
2020-04-02 01:40:23 +0900

    プロジェクト追加

---
+++ VoiroTalk/VoiroTalk/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VoiroTalk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
