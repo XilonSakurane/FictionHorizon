24c751301a3c2b5ecef043b32b2896f4d3fedaf2
XilonSakurane <epsilon_xjmptw0@yahoo.co.jp>
2020-04-02 02:58:43 +0900

    内容整理

--- VoiroTalk/VoiroTalk/Program.cs
+++ VoiroTalk/VoiroTalk/Program.cs
@@ -4,14 +4,9 @@ namespace VoiroTalk
 {
     class Program
     {
-        /// <summary>
-        /// メモ：
-        /// </summary>
-        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            
         }
     }
 }
