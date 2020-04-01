1e62188cc95765971d81a51e338ba2e08cad2177
XilonSakurane <epsilon_xjmptw0@yahoo.co.jp>
2020-04-02 02:25:40 +0900

    フォルダ構成の確立

--- VoiroTalk/VoiroTalk/Program.cs
+++ VoiroTalk/VoiroTalk/Program.cs
@@ -4,9 +4,14 @@ namespace VoiroTalk
 {
     class Program
     {
+        /// <summary>
+        /// メモ：
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            
         }
     }
 }
